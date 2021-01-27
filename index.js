f7c9ee60e146a1460b8b2074d481188f5402a4e4
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-27 23:32:44 +0200

    modularize index.js

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-const myProjects = [
-  {
-    projectName: 'Politico',
-    description: 'Politico enables citizens vote for politicians running for various government offices',
-    repository: 'https://github.com/Blaise-Shyaka/Politico',
-    liveLink: 'https://blaise-shyaka.github.io/Politico/templates/',
-    screenshotSource: './screenshots/politico.png',
-    stack: 'HTML | CSS | Node JS | PostgreSQL'
-  },
-  {
-    projectName: 'AnnounceIT',
-    description: 'AnnounceIT is a solution enabling broadcasting agencies to receive and manage announcements.',
-    repository: 'https://github.com/Blaise-Shyaka/AnnounceIT',
-    liveLink: 'https://blaise-shyaka.github.io/AnnounceIT/templates/',
-    screenshotSource: './screenshots/announceit.png',
-    stack: 'HTML | CSS | Node JS | PostgreSQL'
-  },
-  {
-    projectName: 'Drum-kit',
-    description: 'Drum-kit allows users to play a drum using their keyboard',
-    repository: 'https://github.com/Blaise-Shyaka/drum-kit',
-    liveLink: 'https://blaise-shyaka.github.io/drum-kit/',
-    screenshotSource: './screenshots/drum-kit.png',
-    stack: 'HTML | CSS | JS'
-  },
-  {
-    projectName: 'Politico-react',
-    description: 'An experimental version of the politico app developed using React JS',
-    repository: 'https://github.com/Blaise-Shyaka/politico-react',
-    screenshotSource: './screenshots/politico.png',
-    stack: 'React JS'
-  }
-];
+import { myProjects } from './projects.js'
 
 const displayTemplateOne = (obj) => {
   return `<div class="project card">
---
+++ projects.js
@@ -0,0 +1,33 @@
+export const myProjects = [
+  {
+    projectName: 'Twitter Poets',
+    description: 'Politico enables citizens vote for politicians running for various government offices',
+    repository: 'https://github.com/Blaise-Shyaka/Politico',
+    liveLink: 'https://blaise-shyaka.github.io/Politico/templates/',
+    screenshotSource: './screenshots/politico.png',
+    stack: 'HTML | CSS | Node JS | PostgreSQL'
+  },
+  {
+    projectName: 'AnnounceIT',
+    description: 'AnnounceIT is a solution enabling broadcasting agencies to receive and manage announcements.',
+    repository: 'https://github.com/Blaise-Shyaka/AnnounceIT',
+    liveLink: 'https://blaise-shyaka.github.io/AnnounceIT/templates/',
+    screenshotSource: './screenshots/announceit.png',
+    stack: 'HTML | CSS | Node JS | PostgreSQL'
+  },
+  {
+    projectName: 'Drum-kit',
+    description: 'Drum-kit allows users to play a drum using their keyboard',
+    repository: 'https://github.com/Blaise-Shyaka/drum-kit',
+    liveLink: 'https://blaise-shyaka.github.io/drum-kit/',
+    screenshotSource: './screenshots/drum-kit.png',
+    stack: 'HTML | CSS | JS'
+  },
+  {
+    projectName: 'Politico-react',
+    description: 'An experimental version of the politico app developed using React JS',
+    repository: 'https://github.com/Blaise-Shyaka/politico-react',
+    screenshotSource: './screenshots/politico.png',
+    stack: 'React JS'
+  }
+];

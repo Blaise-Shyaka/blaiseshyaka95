e735c81425c78868b0fc9457b867766214b089ec
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-28 00:01:27 +0200

    remove dummy projects

--- projects.js
+++ projects.js
@@ -1,33 +1,33 @@
 export const myProjects = [
   {
     projectName: 'Twitter Poets',
-    description: 'Politico enables citizens vote for politicians running for various government offices',
+    description: 'This project is a Twitter-like social media app where users share mostly poetic content.',
+    repository: 'https://github.com/Blaise-Shyaka/Twitter-Poets',
+    liveLink: 'https://protected-fjord-05413.herokuapp.com/',
+    screenshotSource: './screenshots/twitter-poets.png',
+    stack: 'Ruby on Rails'
+  },
+  {
+    projectName: 'FindEvent',
+    description: 'FindEvent helps users to search events globally as well as events near them.',
+    repository: 'https://github.com/Blaise-Shyaka/FindEvent',
+    liveLink: 'https://raw.githack.com/Blaise-Shyaka/FindEvent/findevent-ui/dist/index.html',
+    screenshotSource: './screenshots/findevent.png',
+    stack: 'HTML | SCSS | BootStrap'
+  },
+  {
+    projectName: 'Politico',
+    description: 'Politico enables citizens vote for politicians running for offices',
     repository: 'https://github.com/Blaise-Shyaka/Politico',
     liveLink: 'https://blaise-shyaka.github.io/Politico/templates/',
     screenshotSource: './screenshots/politico.png',
-    stack: 'HTML | CSS | Node JS | PostgreSQL'
+    stack: 'HTML | CSS | Node JS | Postgresql'
   },
   {
-    projectName: 'AnnounceIT',
-    description: 'AnnounceIT is a solution enabling broadcasting agencies to receive and manage announcements.',
-    repository: 'https://github.com/Blaise-Shyaka/AnnounceIT',
-    liveLink: 'https://blaise-shyaka.github.io/AnnounceIT/templates/',
-    screenshotSource: './screenshots/announceit.png',
-    stack: 'HTML | CSS | Node JS | PostgreSQL'
-  },
-  {
-    projectName: 'Drum-kit',
-    description: 'Drum-kit allows users to play a drum using their keyboard',
-    repository: 'https://github.com/Blaise-Shyaka/drum-kit',
-    liveLink: 'https://blaise-shyaka.github.io/drum-kit/',
-    screenshotSource: './screenshots/drum-kit.png',
-    stack: 'HTML | CSS | JS'
-  },
-  {
-    projectName: 'Politico-react',
-    description: 'An experimental version of the politico app developed using React JS',
-    repository: 'https://github.com/Blaise-Shyaka/politico-react',
-    screenshotSource: './screenshots/politico.png',
-    stack: 'React JS'
+    projectName: 'Md-Lint',
+    description: 'Md-lint is a linter for files written in a markdown format.',
+    repository: 'https://github.com/Blaise-Shyaka/md-lint',
+    screenshotSource: './screenshots/md-lint.png',
+    stack: 'Ruby'
   }
 ];

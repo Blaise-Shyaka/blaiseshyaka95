120ba7f969a7309e1997cf5613d74dca05e0f60f
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-27 11:01:57 +0200

    format code with a 2 spaces instead of 4

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const myProjects = [
-    {
-        projectName: 'Politico',
-        description: 'Politico enables citizens vote for politicians running for various government offices',
-        repository: 'https://github.com/Blaise-Shyaka/Politico',
-        githubPage: 'https://blaise-shyaka.github.io/Politico/templates/',
-        stack: 'HTML | CSS | Node JS | PostgreSQL'
-    },
-    {
-        projectName: 'AnnounceIT',
-        description: 'AnnounceIT is a solution enabling broadcasting agencies to receive and manage announcements.',
-        repository: 'https://github.com/Blaise-Shyaka/AnnounceIT',
-        githubPage: 'https://blaise-shyaka.github.io/AnnounceIT/templates/',
-        stack: 'HTML | CSS | Node JS | PostgreSQL'
-    },
-    {
-        projectName: 'Drum-kit',
-        description: 'Drum-kit allows users to play a drum using their keyboard',
-        repository: 'https://github.com/Blaise-Shyaka/drum-kit',
-        githubPage: 'https://blaise-shyaka.github.io/drum-kit/',
-        stack: 'HTML | CSS | JS'
-    },
-    {
-        projectName: 'Politico-react',
-        description: 'An experimental version of the politico app developed using React JS',
-        repository: 'https://github.com/Blaise-Shyaka/politico-react',
-        stack: 'React JS'
-    }
+  {
+    projectName: 'Politico',
+    description: 'Politico enables citizens vote for politicians running for various government offices',
+    repository: 'https://github.com/Blaise-Shyaka/Politico',
+    githubPage: 'https://blaise-shyaka.github.io/Politico/templates/',
+    stack: 'HTML | CSS | Node JS | PostgreSQL'
+  },
+  {
+    projectName: 'AnnounceIT',
+    description: 'AnnounceIT is a solution enabling broadcasting agencies to receive and manage announcements.',
+    repository: 'https://github.com/Blaise-Shyaka/AnnounceIT',
+    githubPage: 'https://blaise-shyaka.github.io/AnnounceIT/templates/',
+    stack: 'HTML | CSS | Node JS | PostgreSQL'
+  },
+  {
+    projectName: 'Drum-kit',
+    description: 'Drum-kit allows users to play a drum using their keyboard',
+    repository: 'https://github.com/Blaise-Shyaka/drum-kit',
+    githubPage: 'https://blaise-shyaka.github.io/drum-kit/',
+    stack: 'HTML | CSS | JS'
+  },
+  {
+    projectName: 'Politico-react',
+    description: 'An experimental version of the politico app developed using React JS',
+    repository: 'https://github.com/Blaise-Shyaka/politico-react',
+    stack: 'React JS'
+  }
 ];
